2de37893d33e5274d2bfc63df5d192281373a9b0
Taekyoon <tgchoi03@gmail.com>
2020-06-07 00:58:09 +0900

    Update exec_jupyter_for_gpu.sh

--- exec_jupyter_for_gpu.sh
+++ exec_jupyter_for_gpu.sh
@@ -1,3 +1,3 @@
-sudo docker run -it --rm -p 8889:8889\
+sudo docker run -it --rm -p 8889:8889 --memory=4g --memory-swap=8g\
  --mount type=bind,destination=/workspace/practice,source=`pwd`\
  tf2_practice_gpu
