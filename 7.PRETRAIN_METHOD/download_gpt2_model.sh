8debb3e631de1adc12c6f5010f99914ee066ee8c
Taekyoon <tgchoi03@gmail.com>
2020-09-06 11:54:48 +0900

    Update download_gpt2_model.sh

--- 7.PRETRAIN_METHOD/download_gpt2_model.sh
+++ 7.PRETRAIN_METHOD/download_gpt2_model.sh
@@ -1,14 +1,12 @@
-ZIP_FILE=./gpt_ckpt.zip
-DIR_PATH=./gpt_ckpt
-if test -f "$DIR_PATH"; then
-    if test -f "$ZIP_FILE"; then
-        echo "$ZIP_FILE not exists."
-        wget https://www.dropbox.com/s/nzfa9xpzm4edp6o/gpt_ckpt.zip
-    else
-        echo "$ZIP_FILE exists."
-    fi
-    unzip "$FILE"
-    rm "$FILE"
-else
-    echo "$DIR_PATH exists."
+FILE=./gpt_ckpt.zip
+echo "$FILE download"
+
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else 
+    echo "$FILE does not exist."
+    wget https://www.dropbox.com/s/nzfa9xpzm4edp6o/gpt_ckpt.zip
 fi
+
+unzip "$FILE"
+rm "$FILE"
